03898f5758845d85961d0aa5d8faedd94d7fddb1
mhmd-zbib <mohammad.h.zbib@gmail.com>
2025-06-07 02:34:51 +0300

    init

---
+++ src/main/java/com/munusync/backend/MunusyncApplication.java
@@ -0,0 +1,13 @@
+package com.munusync.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunusyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunusyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/munusync/backend/MunusyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munusync.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunusyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
