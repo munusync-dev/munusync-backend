f0e4a14fd8a4ec85bf13a0afd37c165bc25f5adc
fusion tech <amjadahmadieh5@gmail.com>
2025-07-06 21:12:22 +0300

    create user CRUD operation

---
+++ src/main/java/com/munusync/backend/dtos/UserDto.java
@@ -0,0 +1,16 @@
+package com.munusync.backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDto {
+    private Long id;
+    private String name;
+    private String email;
+}
