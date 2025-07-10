a68eaf4889586ad04b5f9f30a6bd5a15502f9653
Mhmd Shami <mhmddshamii@gmail.com>
2025-07-10 14:59:40 +0300

    final Tweeks :) now it works

--- src/main/java/com/munusync/backend/entity/User.java
+++ src/main/java/com/munusync/backend/entity/User.java
@@ -21,7 +21,6 @@ public class User {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
 }
